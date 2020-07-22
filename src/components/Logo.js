aaeffc35cbc5ac1475b384cb6d3aed7f14188ca8
lohwenyu <e0421622@u.nus.edu>
2020-07-22 18:51:09 +0800

    edit logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
 
 export default class Logo extends Component {
     render() {
         return(
             <View style={styles.container}>
-                <Image style={{width: 414, height: 200}}
+                <Image style={styles.image}
                     source={require('../images/6.png')}/>
                 <Text style={styles.logoText}>Hey there, Shearite!</Text>
 
@@ -19,6 +19,10 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-end',
         alignItems: 'center',
+    },
+    image : {
+        width: Dimensions.get('window').width,
+        height: 200,
     },
     logoText : {
         marginVertical: 15,
